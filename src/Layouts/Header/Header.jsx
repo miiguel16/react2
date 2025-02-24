2274b7e686b4e63b2093f48da320c6ab8cf88169
Sena <dimatesebas73@gmail.com>
2025-02-24 17:52:22 -0500

    .

--- src/Layouts/Header/Header.jsx
+++ src/Layouts/Header/Header.jsx
@@ -5,6 +5,7 @@ export const Header = () => {
     return (
         <header className="bg-blue-400 w-full flex flex-row flex-wrap justify-center items-center h-26 pb-20">
             <h1 className="text-sky-100 font-extrabold text-2xl mt-2 p-8">Introduccion a React 2025</h1>
+            <h1>sidjasodjasli</h1>
             <Navbar />
         </header>
     )
